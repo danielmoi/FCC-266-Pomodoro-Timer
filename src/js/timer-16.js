4ef90e2aef606ab260a6dfd0a431448e3727b3bd
danielmoi <daniel@ampersandmoi.com>
2016-02-07 14:47:32 +1100

    Add finished function

--- src/js/timer-16.js
+++ src/js/timer-16.js
@@ -14,7 +14,6 @@ function MyTimer(mins, container) {
     if (running === 0) {
       referenceTime = Date.now();
       running = 1;
-      self.display(initial_ms);
 
       intervalID = setInterval(function () {
         var checkTime = Date.now(),
@@ -22,6 +21,13 @@ function MyTimer(mins, container) {
         remaining_ms = initial_ms - actualInterval;
 
         self.display(remaining_ms);
+        
+        if (remaining_ms <= 0) {
+          self.display(0);
+          self.stop();
+          self.finished();
+        }
+        
 
 
       }, 250);
@@ -54,8 +60,12 @@ function MyTimer(mins, container) {
     clearInterval(intervalID);
     initial_ms = mins * 1000 * 60;
     self.display(initial_ms);
+    console.log(intervalID);
   };
 
+  self.finished = function () {
+    console.log('finished!');
+  }
 
 
 
@@ -63,7 +73,7 @@ function MyTimer(mins, container) {
 
 
 
-var timer1 = new MyTimer(1, 'timer1');
+var timer1 = new MyTimer(0.2, 'timer1');
 $('.stop').on('click', timer1.stop);
 $('.start').on('click', timer1.start);
 $('.reset').on('click', timer1.reset);
