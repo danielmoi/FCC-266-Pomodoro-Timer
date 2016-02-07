4ee96cd4726b57f652cf08cdcfe18ad5956de5a8
danielmoi <daniel@ampersandmoi.com>
2016-02-07 14:29:49 +1100

    Move display into separate function

--- src/js/timer-16.js
+++ src/js/timer-16.js
@@ -2,7 +2,7 @@ function MyTimer(mins, container) {
   var referenceTime,
     initial_ms = mins * 1000 * 60,
     remaining_ms,
-    display,
+    display = document.getElementById(container),
     display_min,
     display_sec,
     intervalID = false,
@@ -14,25 +14,33 @@ function MyTimer(mins, container) {
     if (running === 0) {
       referenceTime = Date.now();
       running = 1;
+
       intervalID = setInterval(function () {
         var checkTime = Date.now(),
           actualInterval = checkTime - referenceTime;
         remaining_ms = initial_ms - actualInterval;
+        
+        self.display(remaining_ms);
+
+//        if (actualInterval < 750) {
+//          console.log("hello");
+//          remaining_ms += 750;
+//        }
 
 
-        display = document.getElementById(container);
 
-        display_min = Math.floor(remaining_ms / 60000);
-        display_sec = Math.floor(remaining_ms / 1000) % 60;
-        display_sec = display_sec < 10 ? '0' + display_sec : display_sec;
-        display_min = display_min < 10 ? '0' + display_min : display_min;
-        display.innerHTML = display_min + ":" + display_sec;
       }, 250);
-      console.log(intervalID);
     }
 
   };
 
+  self.display = function (time) {
+    display_min = Math.floor(time / 60000);
+    display_sec = Math.floor(time / 1000) % 60;
+    display_sec = display_sec < 10 ? '0' + display_sec : display_sec;
+    display_min = display_min < 10 ? '0' + display_min : display_min;
+    display.innerHTML = display_min + ":" + display_sec;
+  };
 
 
 
@@ -41,9 +49,13 @@ function MyTimer(mins, container) {
       running = 0;
       initial_ms = remaining_ms;
       clearInterval(intervalID);
-      console.log(intervalID);
-      console.log(remaining_ms);
     }
+  };
+
+  self.reset = function () {
+    running = 0;
+    clearInterval(intervalID);
+    initial_ms = mins * 1000 * 60;
   };
 
 
@@ -56,3 +68,4 @@ function MyTimer(mins, container) {
 var timer1 = new MyTimer(1, 'timer1');
 $('.stop').on('click', timer1.stop);
 $('.start').on('click', timer1.start);
+$('.reset').on('click', timer1.reset);
